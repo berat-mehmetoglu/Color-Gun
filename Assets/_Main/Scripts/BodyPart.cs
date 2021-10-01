5a2a31a5829c17501c6ba275e3964747491b76f3
SerefTahmazVirtualProjects <76683083+SerefTahmazVirtualProjects@users.noreply.github.com>
2021-10-01 16:32:27 +0300

    [Added] NPC

--- Assets/_Main/Scripts/BodyPart.cs
+++ Assets/_Main/Scripts/BodyPart.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class BodyPart : MonoBehaviour
 {
-    [SerializeField] private MortyColor _color;
+    [SerializeField] private Renderer _rend;
+    [SerializeField] private int _konum;
+    
     private NPCEvents _npcEvents;
+    private NPCController _controller;
 
+    private bool _deathPart=false;
     // Start is called before the first frame update
     void Start()
     {
         _npcEvents = GetComponentInParent<NPCEvents>();
+        _controller = GetComponentInParent<NPCController>();
     }
 
     public void OnShoot(MortyColor color)
     {
-        if (color == _color)
+        if (_deathPart)
         {
-            _npcEvents.InvokeNPCDeath();
+            return;
+        }
+        
+        if (color == _controller._mortyDic[_rend.materials[_konum]])
+        {
+            foreach (var VARIABLE in _controller._eyes)
+            {
+                if (color == _controller._mortyDic[VARIABLE.material])
+                {
+                    _npcEvents.InvokeNPCDeath();
+                    _deathPart = true;
+                    break;
+                }
+            }
         }
     }
 }
@@ -28,5 +46,5 @@ public enum MortyColor
     Yellow,
     Blue,
     Orange,
-    Purple,
+    Purple
 }
--- Assets/_Main/Scripts/NPCAnimatorController.cs
+++ Assets/_Main/Scripts/NPCAnimatorController.cs
@@ -40,3 +40,57 @@ public enum MortyAnim
     Run,
     Shoot
 }
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    [SerializeField] private Renderer _rend;
+    [SerializeField] private int _konum;
+    
+    private NPCEvents _npcEvents;
+    private NPCController _controller;
+
+    private bool _deathPart=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _npcEvents = GetComponentInParent<NPCEvents>();
+        _controller = GetComponentInParent<NPCController>();
+    }
+
+    public void OnShoot(MortyColor color)
+    {
+        if (_deathPart)
+        {
+            return;
+        }
+        
+        if (color == _controller._mortyDic[_rend.materials[_konum]])
+        {
+            foreach (var VARIABLE in _controller._eyes)
+            {
+                if (color == _controller._mortyDic[VARIABLE.material])
+                {
+                    _npcEvents.InvokeNPCDeath();
+                    _deathPart = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+public enum MortyColor
+{
+    Blue,
+    Purple,
+    Yellow,
+    Green,
+    Red
+}
+
+ */
